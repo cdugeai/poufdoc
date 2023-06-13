68f2a2b24b62c9ebaa55a57059875cf132d584c7
Serena Gruarin <73535961+SereGrn@users.noreply.github.com>
2023-06-13 17:29:26 +0200

    Update du script des graphiques

--- data/Script_graphiques.R
+++ data/Script_graphiques.R
@@ -1,8 +1,8 @@
 library(readr)
 library(dplyr)
+library(tidyr)
 library(stringr)
 library(ggplot2)
-
 library(aws.s3)
 
 
@@ -11,17 +11,44 @@ download.file(url, "dataset_emplois.csv")
 
 df <- read_delim("dataset_emplois.csv", delim =";")
 
-df_num <- df %>% filter(str_detect(MÃ©tier , "Data engineer|Data Scientist|Analyste de donnÃ©es|Administratrice de bases de donnÃ©es|Analyste des donnÃ©es" )) %>%
-  mutate(`Organisme de rattachement` = ifelse(str_detect(`Organisme de rattachement`, "MinistÃ¨re des ArmÃ©es"), "MinistÃ¨re des ArmÃ©es", `Organisme de rattachement` ))
+df_num <- df %>% filter(str_detect(Métier , "Data engineer|Data Scientist|Analyste de données|Administratrice de bases de données|Analyste des données" )) %>%
+  mutate(`Organisme de rattachement` = ifelse(str_detect(`Organisme de rattachement`, "Ministère des Armées"), "Ministère des Armées", `Organisme de rattachement` ))
 
-nat_contrat <- group_by(df_num, `Nature de contrat`) %>% 
-  reframe(., count=n()) %>% 
-  arrange(., desc(count)) 
+# 1er graphique 
+graph_1 <- df_num %>%  replace_na(list(`Nature de contrat` = "Non renseigné")) %>%
+  ggplot(aes(x = reorder(`Nature de contrat`, -table(`Nature de contrat`)[`Nature de contrat`]), fill = `Nature de contrat`)) +
+  geom_bar(width = 0.3) + labs(x = "Nature de contrat", y = "Nombre d'offres proposées") + 
+  theme(axis.text.x=element_text(angle=50, hjust=1), plot.title = element_text(hjust = 0.5)) +
+  ggtitle("Répartition des offres publiées par nature de contrat") 
 
-graph_1 <- ggplot(data=nat_contrat, aes(x=reorder(`Nature de contrat`,count), y=count)) +
-  geom_bar(aes(y=count, fill=`Nature de contrat`), stat="identity", width=0.5)+
-  labs(x= 'Nature de contrat', y = "Nombre d\'offres proposÃ©es")
+ggsave("graph1.png")
 
-ggsave("myplot.png")
+put_object(file = "graph1.png", object = "graph1.png", bucket = "sgruarin", region = "")
 
-put_object(file = "myplot.png", object = "myplot.png", bucket = "projet-poufdoc", region = "")
+# 2ème graphique 
+graph_2 <- df_num %>% ggplot(aes(x = reorder(Versant, -table(Versant)[Versant]), fill = Versant)) +
+  geom_bar(width = 0.3) +
+  labs(x = "Nature de la fonction publique", y = "Nombre d'offres proposées") + 
+  theme(axis.text.x=element_text(angle=50, hjust=1), plot.title = element_text(hjust = 0.5)) +
+  ggtitle("Répartition des offres publiées par nature de la fonction publique") 
+
+ggsave("graph2.png")
+
+put_object(file = "graph2.png", object = "graph2.png", bucket = "sgruarin", region = "")
+
+# 3ème graphique
+nb_orga <- df_num %>%  group_by(`Organisme de rattachement`) %>% tally(sort = TRUE) %>% slice(1:5)
+
+
+graph_3 <- ggplot(nb_orga, aes(x="", y=n, fill=`Organisme de rattachement`)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  geom_text(aes(label = paste0(n)), position = position_stack(vjust=0.5)) + theme_void() +
+  ggtitle("Représentation des cinq organisme les plus demandeurs")
+
+
+ggsave("graph3.png")
+
+put_object(file = "graph3.png", object = "graph3.png", bucket = "sgruarin", region = "")
+
+
