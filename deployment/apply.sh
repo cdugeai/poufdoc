70619feff46af6cff258e1eb7b93412d8caa3565
cdugeai <colin.dugeai@modernisation.gouv.fr>
2023-06-13 14:40:01 +0000

    ajout scripts de creation des ressources kube

---
+++ deployment/apply.sh
@@ -0,0 +1,4 @@
+kubectl apply -f pvc.yaml
+kubectl apply -f service.yaml
+kubectl apply -f statefulset.yaml
+kubectl apply -f ingress.yaml
---
+++ deployment/delete.sh
@@ -0,0 +1,4 @@
+kubectl delete -f ingress.yaml
+kubectl delete -f service.yaml
+kubectl delete -f statefulset.yaml
+kubectl delete -f pvc.yaml
